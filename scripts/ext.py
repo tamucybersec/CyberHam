4efbe623feaaffe543310d4bf9fe1a6df10e99ea
Eric Yang <erichyang11@gmail.com>
2022-09-20 23:25:24 -0500

    organize meeting codes

--- scripts/ext.py
+++ scripts/ext.py
@@ -4,8 +4,11 @@ import sys
 import pandas as pd
 project_path = Path(__file__).parent
 
+meeting_codes = ("OSDFT", "OZHIW", "WBYAG", "AWDAA")
+
 conn = sqlite3.connect(project_path.parent.joinpath("cyberham").joinpath("db").joinpath("attendance.db"))
 c = conn.cursor()
+
 
 def ncl_voucher(csv):
     df = pd.read_csv(csv)
@@ -20,11 +23,11 @@ def ncl_voucher(csv):
         events = c.fetchall()
 
         for event in events:
-            if event[0] in ("OSDFT", "OZHIW", "WBYAG", "AWDAA"):
+            if event[0] in meeting_codes:
                 count += 1
 
         print(f"<@{_id[0]}> {_id[1]} {email} - {count}")
 
 if __name__ == '__main__':
     if (sys.argv[1] == 'ncl'):
-        ncl_voucher(sys.argv[2])
+        ncl_voucher(sys.argv[2], sys.argv[3:])
