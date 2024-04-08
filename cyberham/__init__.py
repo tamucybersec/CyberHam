1b058d517e1f90fa2e3744841c826ed7fb527539
Stella Yang <43301578+lilacstella@users.noreply.github.com>
2024-04-07 19:51:58 -0500

    use new config format (#27)
    made new ElasticsearchConfig class to pass config parameters easily
    
    Co-authored-by: HomeoStasis69 <128863300+HomeoStasis69@users.noreply.github.com>

--- cyberham/__init__.py
+++ cyberham/__init__.py
@@ -1,5 +1,6 @@
 import tomllib
 import logging
+from datetime import datetime
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -7,23 +8,56 @@ import sqlite3
 
 project_path = Path(__file__).parent
 
+# load config.toml
 with open(project_path.parent.joinpath("config.toml"), "rb") as f:
     data = tomllib.load(f)
 
+# connecting to local sqlite db
 conn = sqlite3.connect(project_path.joinpath("db").joinpath("attendance.db"))
 c = conn.cursor()
 
+# loading tokens
 google_token = project_path.joinpath("secrets").joinpath("token.json")
-client_secret = project_path.joinpath("secrets").joinpath(data["google_client"])
-discord_token = data["discord_token"]
+client_secret = project_path.joinpath("secrets").joinpath(data["google"]["client_file_name"])
+discord_token = data["discord"]["token"]
 
+
+# discord outputs configs
 @dataclass
 class Guild:
     id: int
 
 
-guild_id = [Guild(x) for x in data["test_guild_ids"]]
-admin_channel_id = data["admin_channel_id"]
+guild_id = [Guild(x) for x in data["discord"]["test_guild_ids"]]
+admin_channel_id = data["discord"]["admin_channel_id"]
+
+
+# elasticsearch configs
+@dataclass
+class ElasticsearchConfig:
+    api_key: str
+    id: str
+    endpoints: str
+    index_postfix: str
+
+
+es_data = data["elasticsearch"]
+
+# Check if 'index_postfix' exists in Elasticsearch data
+if "index_postfix" in es_data:
+    es_index_postfix = es_data["index_postfix"]
+else:
+    # If 'index_postfix' does not exist, use the current year
+    es_index_postfix = str(datetime.now().year)
+
+# Create Elasticsearch configuration
+es_conf = ElasticsearchConfig(
+    api_key=es_data["api_key"],
+    id=es_data["id"],
+    endpoints=es_data["endpoints"],
+    index_postfix=es_index_postfix
+)
+
 
 # config discord logging
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
