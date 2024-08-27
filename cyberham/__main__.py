20f5c50c3d214ded4efeba9062a3b2c4f015186f
deciphr <24928894+deciphr@users.noreply.github.com>
2024-08-26 19:21:16 -0500

    Fixed map hyperlink
    It's possible that the location for the event may not be a 3-4 character building code.

--- cyberham/__main__.py
+++ cyberham/__main__.py
@@ -580,8 +580,14 @@ Here's what we have for this week
 
             for location, events in locations.items():
                 if len(events) > 0:
-                    boilerplate += f"**{location}** ([Map](<https://aggiemap.tamu.edu/map/d?bldg={location.split(' ')[0]}>))\n"
-                    
+                    bldg = location.split(" ")[0]
+
+                    # hacky method
+                    if len(bldg) == 3 or len(bldg) == 4:
+                        boilerplate += f"**{location}** ([Map](<https://aggiemap.tamu.edu/map/d?bldg={location.split(' ')[0]}>))\n"
+                    else:
+                        boilerplate += f"**{location}**\n"
+                        
                     for event in events:
                         start_time = event.start_time.astimezone(timezone('US/Central')).strftime("%I:%M%p").lstrip("0").replace(" 0", " ")
                         end_time = event.end_time.astimezone(timezone('US/Central')).strftime("%I:%M%p").lstrip("0").replace(" 0", " ")
